742c2c6d1f8610347ab91e34a445f03ff99412d3
john <sato.keita@furyu.jp>
2018-06-17 05:23:12 +0900

    スタンプを押した履歴を保存できるようにした

--- app/src/main/java/com/example/satohjohn/kotlinsample/activity/MovieEditorActivity.kt
+++ app/src/main/java/com/example/satohjohn/kotlinsample/activity/MovieEditorActivity.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import android.content.Intent
 import android.media.SoundPool
 import android.os.Bundle
-import android.os.Environment
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
@@ -16,6 +15,7 @@ import com.example.satohjohn.kotlinsample.data.Stamp
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import kotlinx.android.synthetic.main.activity_movie_editor.*
 import java.util.*
+import kotlin.collections.ArrayList
 
 class MovieEditorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,19 +27,23 @@ class MovieEditorActivity : AppCompatActivity() {
         Log.d("MovieEditor", "${resourceId}")
 
         (findViewById<View>(R.id.sample_start_button) as Button).setOnClickListener({
+            stampList.clear()
+            musicStartTime = System.currentTimeMillis()
             soundPool.play(resourceId, 1.0f, 1.0f, 0, 0, 1.0f);
         })
         val shardPreferences = this.getPreferences(Context.MODE_PRIVATE)
 
         (findViewById<View>(R.id.movie_save_button) as Button).setOnClickListener({
-            // jsonを保存する
+            if (backgroundImageUrl.isEmpty()) {
+                return@setOnClickListener
+            }
+
             val mapper     = jacksonObjectMapper()
             val createdMovie = CreatedMovie(
-                    listOf(
-                            Stamp("", 400, 199, 200),
-                            Stamp("", 1300, 199, 200)
-                    ),
-                    "",
+                    stampList.map {
+                        Stamp(it.id, it.time)
+                    },
+                    backgroundImageUrl,
                     musicId)
             val jsonString = mapper.writeValueAsString(createdMovie)
             Log.d("movieEditor", jsonString)
@@ -47,6 +51,25 @@ class MovieEditorActivity : AppCompatActivity() {
             val shardPrefEditor = shardPreferences.edit()
             shardPrefEditor.putString("${Date()}", jsonString)
             shardPrefEditor.apply()
+        })
+
+        testButton1.setOnClickListener({
+            stampList.add(Stamp(
+                    "3mi",
+                    System.currentTimeMillis() - musicStartTime
+            ))
+        })
+        testButton2.setOnClickListener({
+            stampList.add(Stamp(
+                    "2re",
+                    System.currentTimeMillis() - musicStartTime
+            ))
+        })
+        testButton3.setOnClickListener({
+            stampList.add(Stamp(
+                    "1do",
+                    System.currentTimeMillis() - musicStartTime
+            ))
         })
 
         Log.d("MovieEditor", "${shardPreferences.all}")
@@ -61,16 +84,22 @@ class MovieEditorActivity : AppCompatActivity() {
         })
     }
 
+    var musicStartTime: Long = System.currentTimeMillis()
+    val stampList: ArrayList<Stamp> = ArrayList()
+    var backgroundImageUrl: String = ""
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == 6542) {
             Log.d("movieEditor", "${data}")
             if (resultCode != Activity.RESULT_OK || data == null) {
                 return
             }
+            backgroundImageUrl = data.data.path
             backgroundImageView.setImageURI(data.data)
         }
 
     }
 
     private val soundPool = SoundPool.Builder().setMaxStreams(7).build()
+
 }
