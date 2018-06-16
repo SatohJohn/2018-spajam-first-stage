4e06e7bffa3685186239729bc395b06f5efc9f9e
john <sato.keita@furyu.jp>
2018-06-16 17:06:56 +0900

    音を鳴らせるようにした

--- app/src/main/java/com/example/satohjohn/kotlinsample/activity/MainActivity.kt
+++ app/src/main/java/com/example/satohjohn/kotlinsample/activity/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.satohjohn.kotlinsample.activity
 
 import android.content.Intent
+import android.graphics.Point
+import android.media.SoundPool
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.Display
 import android.view.Menu
 import android.view.MenuItem
+import android.view.MotionEvent
+import android.widget.Button
 import com.example.satohjohn.kotlinsample.R
 
 import kotlinx.android.synthetic.main.activity_main.*
@@ -31,8 +36,27 @@ class MainActivity : AppCompatActivity() {
         sample_text.setOnClickListener { view ->
             startActivity(Intent(this, TestActivity::class.java))
         }
+
+        piano = piano.map { soundPool.load(this, it, 1) }
+
+        defaultDisplay = getWindowManager().getDefaultDisplay();
+
         Log.d("mainActivity", "end on create")
     }
+
+    private val soundPool = SoundPool.Builder().setMaxStreams(7).build()
+    private var defaultDisplay: Display? = null
+
+
+    private var piano = listOf(
+            R.raw.se_maoudamashii_instruments_piano2_1do,
+            R.raw.se_maoudamashii_instruments_piano2_2re,
+            R.raw.se_maoudamashii_instruments_piano2_3mi,
+            R.raw.se_maoudamashii_instruments_piano2_4fa,
+            R.raw.se_maoudamashii_instruments_piano2_5so,
+            R.raw.se_maoudamashii_instruments_piano2_6ra,
+            R.raw.se_maoudamashii_instruments_piano2_7si)
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -47,6 +71,22 @@ class MainActivity : AppCompatActivity() {
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        return event.run {
+            this?.let {
+                //                val position: Int = (it.getY() % piano.size + 0.5f).toInt()
+                // windowの高さ / piano.size
+                val point = Point()
+                defaultDisplay?.getSize(point)
+                val position: Int = (it.getY() / (point.y / piano.size)).toInt()
+                Log.d("mainActivity", "point x: ${it.getX()}, y: ${it.getY()}, point: ${position}, ")
+                soundPool.play(piano[position], 1.0f, 1.0f, 0, 0, 1.0f)
+            }
+
+            super.onTouchEvent(event)
         }
     }
 
@@ -69,4 +109,6 @@ class MainActivity : AppCompatActivity() {
             System.loadLibrary("native-lib")
         }
     }
+
+
 }
