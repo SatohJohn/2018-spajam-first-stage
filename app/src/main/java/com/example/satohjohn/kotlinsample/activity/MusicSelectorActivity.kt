40444c3174042ad17518bc6d601320a005a52a57
john <sato.keita@furyu.jp>
2018-06-17 01:37:15 +0900

    音楽のリストを表示した

--- app/src/main/java/com/example/satohjohn/kotlinsample/activity/MusicSelectorActivity.kt
+++ app/src/main/java/com/example/satohjohn/kotlinsample/activity/MusicSelectorActivity.kt
@@ -3,13 +3,24 @@ package com.example.satohjohn.kotlinsample.activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.View
+import android.widget.ListView
 import com.example.satohjohn.kotlinsample.R
+import com.example.satohjohn.kotlinsample.adapter.MusicListAdapter
 
 class MusicSelectorActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d("Activity", "on create")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_music_selector)
+
+        val musicList = listOf(
+                MusicListAdapter.Music("運命"),
+                MusicListAdapter.Music("やあ"),
+                MusicListAdapter.Music("hoge")
+        )
+        val listView: ListView = findViewById(R.id.music_list) as ListView
+        listView.adapter = MusicListAdapter(this, musicList)
     }
 
 
