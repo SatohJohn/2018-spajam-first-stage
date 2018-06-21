a241eae4b79b5e9a25281dad77a601e41a05c9dd
Tetsuya Kita <kita.tetsuya@furyu.jp>
2018-06-21 09:27:50 +0900

    リソースの参照パスが間違えていたので修正

--- app/src/main/java/com/example/satohjohn/kotlinsample/activity/MusicSelectorActivity.kt
+++ app/src/main/java/com/example/satohjohn/kotlinsample/activity/MusicSelectorActivity.kt
@@ -17,12 +17,12 @@ class MusicSelectorActivity: AppCompatActivity() {
         setContentView(R.layout.activity_music_selector)
 
         val musicList = listOf(
-                Music("運命", R.raw.destiny, R.drawable.music_btn_01_on),
-                Music("やあ", R.raw.destiny, R.drawable.music_btn_02),
-                Music("hoge", R.raw.destiny, R.drawable.music_btn_03),
-                Music("hoge", R.raw.destiny, R.drawable.music_btn_04),
-                Music("hoge", R.raw.destiny, R.drawable.music_btn_05),
-                Music("hoge", R.raw.destiny, R.drawable.music_btn_06)
+                Music("運命", R.raw.destiny, R.mipmap.music_btn_01_on),
+                Music("やあ", R.raw.destiny, R.mipmap.music_btn_02),
+                Music("hoge", R.raw.destiny, R.mipmap.music_btn_03),
+                Music("hoge", R.raw.destiny, R.mipmap.music_btn_04),
+                Music("hoge", R.raw.destiny, R.mipmap.music_btn_05),
+                Music("hoge", R.raw.destiny, R.mipmap.music_btn_06)
         )
         val gridView: GridView = findViewById(R.id.music_list) as GridView
         gridView.adapter = MusicListAdapter(this, musicList)
