12addc3e78cb3e93420c7aa85c22082aabe0e0cd
john <sato.keita@furyu.jp>
2018-06-23 12:48:09 +0900

    top activityを立ち上げたときにpermissionを尋ねるようにした

--- app/src/main/java/com/example/satohjohn/kotlinsample/activity/TopActivity.kt
+++ app/src/main/java/com/example/satohjohn/kotlinsample/activity/TopActivity.kt
@@ -1,14 +1,21 @@
 package com.example.satohjohn.kotlinsample.activity
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import com.example.satohjohn.kotlinsample.R
+import java.util.*
 
 class TopActivity : AppCompatActivity() {
+
+    private val RECORD_REQUEST_CODE = 101
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d("mainActivity", "on create")
@@ -26,7 +33,23 @@ class TopActivity : AppCompatActivity() {
             startActivity(Intent(this, MovieSelectorActivity::class.java))
         })
 
+        setupPermissions()
         Log.d("mainActivity", "end on create")
     }
 
+    private fun setupPermissions() {
+        val permissionGrantedList: List<String> = arrayOf(
+                Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.CAMERA,
+                Manifest.permission.INTERNET,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                .filter {
+                    PackageManager.PERMISSION_GRANTED != ContextCompat.checkSelfPermission(this, it)
+                }
+        if (permissionGrantedList.isNotEmpty()) {
+            Log.i(this::class.java.simpleName, "permission set up: ${permissionGrantedList}")
+            ActivityCompat.requestPermissions(this, permissionGrantedList.toTypedArray(), RECORD_REQUEST_CODE)
+        }
+    }
 }
